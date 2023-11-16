6fa2c340cb8b2488f838655f7333f70ddbf4546b
italochagas <felipe.italo@hotmail.com>
2023-11-16 20:49:49 -0300

    feat: apollo provided placed on layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { CHARACTERS_QUERY } from "@/graphql/query/characters";
+import { CharactersResponse } from "@/types/characters";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+
 const Home = () => {
+  const { data } = useSuspenseQuery<CharactersResponse>(CHARACTERS_QUERY, {
+    variables: { page: 1 },
+  });
+  console.log("Dados", data.characters.results);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home page</h1>
     </div>
   );
 };
