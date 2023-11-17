5813130a149c49c89c47246f733eea1c381e8c7e
italochagas <felipe.italo@hotmail.com>
2023-11-17 12:47:49 -0300

    feat: added fonts and global style

---
+++ src/fonts/passionOne.ts
@@ -0,0 +1,6 @@
+import { Passion_One } from "next/font/google";
+
+export const passionOne = Passion_One({
+  weight: '700',
+  subsets: ['latin'],
+});
---
+++ src/fonts/rowdies.ts
@@ -0,0 +1,7 @@
+import { Rowdies } from "next/font/google";
+
+
+export const rowdies = Rowdies({
+  weight: '700',
+  subsets: ['latin'],
+});
