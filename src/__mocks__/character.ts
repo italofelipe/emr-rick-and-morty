930a6dcc201962b5569e5752a400753a80b212f1
italochagas <felipe.italo@hotmail.com>
2023-11-19 12:02:26 -0300

    test: mock created

---
+++ src/__mocks__/character.ts
@@ -0,0 +1,22 @@
+export const characterResponseMock = {
+
+  gender: "Male",
+  id: "1",
+  image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  name: "Morty Smith",
+  species: "Human",
+  status: "Alive",
+  origin: {
+    dimension: null,
+    name: "unknown",
+    created: null
+  },
+  location: {
+    dimension: "unknown",
+    id: "3",
+    name: "Citadel of Ricks",
+    type: "Space station",
+  }
+
+};
+
